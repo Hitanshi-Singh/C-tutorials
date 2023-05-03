9680fbd627e060b67b73811af9b2e13ed1d9a93d
Hitanshi-Singh <tech.hitanshi@gmail.com>
2023-05-03 07:43:05 +0530

    area of sq

--- area_of_sq.c
+++ area_of_sq.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    int a;
+    float a;
     printf("enter the side of sqaure: ");
-    scanf("%d", &a);
-    printf("the area of the square is: %d", a * a);
+    scanf("%f", &a);
+    printf("the area of the square is: %f", a * a);
     return 0;
 }
