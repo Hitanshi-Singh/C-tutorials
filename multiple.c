28a88a8d8ec75506ebc099f859cf5b54826e4a10
Hitanshi-Singh <tech.hitanshi@gmail.com>
2023-05-05 12:18:01 +0530

    chapter 4

---
+++ multiple.c
@@ -0,0 +1,16 @@
+// Keep taking numbers as input from the user until user enters a number that is a multiple of 7
+
+#include <stdio.h>
+int main()
+{
+  int num;
+  do
+  {
+
+    printf("enter the number: \n");
+    scanf("%d", &num);
+    printf("%d \n", num);
+  } while (num % 7 != 0);
+
+  return 0;
+}
