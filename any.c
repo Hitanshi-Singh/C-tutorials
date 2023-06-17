2727ffb37e302356585b7eda94967aef1534eb00
Hitanshi-Singh <tech.hitanshi@gmail.com>
2023-06-17 23:18:21 +0530

    any

--- any.c
+++ any.c
@@ -3,5 +3,5 @@ void main()
 {
     printf("hey there just trying to save my streak");
     printf("again a exam tom");
-    printf("ethnic sday tom bro")
+    printf("ethnic sday tom bro i am really tired yaaar");
 }
